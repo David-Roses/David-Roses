bfc5f5ae18bcf26e9a73f7b6ca36eafc83932be4
davidroses02 <davidrosasalcaraz@gmail.com>
2022-02-28 20:38:11 +0100

    feat: menu arreglado

--- js/main.js
+++ js/main.js
@@ -7,6 +7,16 @@
     const init = function() {
         let h4 = document.querySelector("body > main > section > article.skills > div.formacion > div.estudios > div.no_oficiales > h4");
         h4.addEventListener("click", accion);
+
+        let elements = document.querySelectorAll("body > header.cabecera > nav > ul > li > a");
+        elements.forEach(element => {
+            element.addEventListener("click", function() {
+                let menuppal = document.querySelector("body > header.cabecera > nav");
+                let hamburguer = document.querySelector("body > header.cabecera > div");
+                menuppal.classList.remove("is_active");
+                hamburguer.classList.remove("is-active");
+            });
+        });
     }
 
     const accion = function(element) {
